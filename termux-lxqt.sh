888e81f3c1e179d399088222bf26790160011fa4
wahasa <69626847+wahasa@users.noreply.github.com>
2022-08-22 14:54:51 +0700

    Update termux-lxqt.sh

--- termux-lxqt.sh
+++ termux-lxqt.sh
@@ -13,19 +13,20 @@ pkg install lxqt* tigervnc -y
 echo "vncserver -geometry 1600x900 -xstartup ../usr/bin/startlxqt" > $PREFIX/bin/desktop-lxqt
 echo "vncserver -kill :1" > $PREFIX/bin/desktop-stop
 
-rm -rf ~/.vnc/xstartup
-echo "#!/bin/bash
-xrdb $HOME/.Xresources
-startlxqt" > ~/.vnc/xstartup
-
 chmod +x $PREFIX/bin/desktop-lxqt
 chmod +x $PREFIX/bin/desktop-stop
-chmod +x ~/.vnc/xstartup
 clear
 
 echo " "
 echo "Command to start 'desktop-lxqt' and stop 'desktop-stop'"
 echo " "
 bash desktop-lxqt
+
+rm -rf ~/.vnc/xstartup
+
+echo "#!/bin/bash
+xrdb $HOME/.Xresources
+startlxqt" > ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
 
 rm termux-lxqt.sh
