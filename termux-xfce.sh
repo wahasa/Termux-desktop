4d59147b8a9cf38dd7eb703acff6093173bd6ddb
wahasa <69626847+wahasa@users.noreply.github.com>
2022-08-22 14:29:18 +0700

    Update termux-xfce.sh

--- termux-xfce.sh
+++ termux-xfce.sh
@@ -12,22 +12,20 @@ pkg install neofetch -y
 pkg install xfce4* tigervnc -y
 echo "vncserver -geometry 1600x900 -xstartup ../usr/bin/startxfce4" > $PREFIX/bin/desktop-xfce
 echo "vncserver -kill :1" > $PREFIX/bin/desktop-stop
-
-rm -rf ~/.vnc/xstartup
-
-touch ~/.vnc/xstartup
-echo "#!/bin/bash
-xrdb $HOME/.Xresources
-startxfce4" > ~/.vnc/xstartup
-
 chmod +x $PREFIX/bin/desktop-xfce
 chmod +x $PREFIX/bin/desktop-stop
-chmod +x ~/.vnc/xstartup
 clear
 
 echo " "
 echo "Command to start 'desktop-xfce' and stop 'desktop-stop'"
 echo " "
 bash desktop-xfce
+
+rm -rf ~/.vnc/xstartup
+
+echo "#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4" > ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
 
 rm termux-xfce.sh
