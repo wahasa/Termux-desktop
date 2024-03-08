40ed67830fcdf859d1b483576671391bea210cd0
wahasa <69626847+wahasa@users.noreply.github.com>
2024-03-08 08:18:45 +0700

    termux-mate.sh

--- Trash/termux-mate.sh
+++ Trash/termux-mate.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
 #preparation to installing
-
 termux-setup-storage
-
 pkg install root-repo x11-repo
 pkg install neofetch -y
 
 #installing mate desktop
-
 pkg install mate* libmate* caja marco tigervnc -y
 
 echo "vncserver -geometry 1600x900 -name remote-desktop :1" > $PREFIX/bin/desktop-mate
@@ -20,15 +16,11 @@ xrdb $HOME/.Xresources
 mate-session" > ~/.vnc/xstartup
 
 chmod +x ~/.vnc/xstartup
-chmod +x $PREFIX/bin/desktop-mate
-chmod +x $PREFIX/bin/desktop-stop
-
-clear
-echo " "
-echo "Vnc Server address will run at 127.0.0.1:5901"
-echo " "
-echo "Command to start 'desktop-mate' and stop 'desktop-stop'"
-echo " "
-
+chmod +x $PREFIX/bin/vnc*
+  clear
+  echo " "
+  echo "Vnc Server address will run at 127.0.0.1:5901"
+  echo " "
+  echo "Command to start 'vnc-start' and stop 'vnc-stop'"
+  echo " "
 rm termux-mate.sh
-bash desktop-mate
