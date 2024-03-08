655eb81390fc1173db519ec7f9f3bb12dee9a259
wahasa <69626847+wahasa@users.noreply.github.com>
2024-03-08 08:14:57 +0700

    termux-lxqt.sh

--- termux-lxqt.sh
+++ termux-lxqt.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
 #preparation to installing
-
 termux-setup-storage
-
 pkg install root-repo x11-repo
 pkg install neofetch -y
 
 #installing lxqt desktop
-
 pkg install lxqt* tigervnc -y
 
-echo "vncserver -geometry 1600x900 -name remote-desktop :1" > $PREFIX/bin/desktop-lxqt
-echo "vncserver -kill :1" > $PREFIX/bin/desktop-stop
+echo "vncserver -geometry 1600x900 -name remote-desktop :1" > $PREFIX/bin/vnc-start
+echo "vncserver -kill :1" > $PREFIX/bin/vnc-stop
 
 mkdir ~/.vnc
 echo "#!/bin/bash
@@ -20,15 +16,11 @@ xrdb $HOME/.Xresources
 startlxqt" > ~/.vnc/xstartup
 
 chmod +x ~/.vnc/xstartup
-chmod +x $PREFIX/bin/desktop-lxqt
-chmod +x $PREFIX/bin/desktop-stop
-
-clear
-echo " "
-echo "Vnc Server address will run at 127.0.0.1:5901"
-echo " "
-echo "Command to start 'desktop-lxqt' and stop 'desktop-stop'"
-echo " "
-
+chmod +x $PREFIX/bin/vnc*
+  clear
+  echo " "
+  echo "Vnc Server address will run at 127.0.0.1:5901"
+  echo " "
+  echo "Command to start 'vnc-start' and stop 'vnc-stop'"
+  echo " "
 rm termux-lxqt.sh
-bash desktop-lxqt
