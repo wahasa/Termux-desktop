10c677142f2c165dac85fd40c87d713f7cd41698
wahasa <69626847+wahasa@users.noreply.github.com>
2024-03-08 08:12:34 +0700

    termux-xfce.sh

--- termux-xfce.sh
+++ termux-xfce.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
 #preparation to installing
-
 termux-setup-storage
-
 pkg install root-repo x11-repo
 pkg install neofetch -y
 
 #installing xfce desktop
-
 pkg install xfce* libxfce* mousepad tigervnc -y
 
-echo "vncserver -geometry 1600x900 -name remote-desktop :1" > $PREFIX/bin/desktop-xfce
-echo "vncserver -kill :1" > $PREFIX/bin/desktop-stop
+echo "vncserver -geometry 1600x900 -name remote-desktop :1" > $PREFIX/bin/vnc-start
+echo "vncserver -kill :1" > $PREFIX/bin/vnc-stop
 
 mkdir ~/.vnc
 echo "#!/bin/bash
@@ -20,15 +16,11 @@ xrdb $HOME/.Xresources
 startxfce4" > ~/.vnc/xstartup
 
 chmod +x ~/.vnc/xstartup
-chmod +x $PREFIX/bin/desktop-xfce
-chmod +x $PREFIX/bin/desktop-stop
-
-clear
-echo " "
-echo "Vnc Server address will run at 127.0.0.1:5901"
-echo " "
-echo "Command to start 'desktop-xfce' and stop 'desktop-stop'"
-echo " "
-
+chmod +x $PREFIX/bin/vnc*
+  clear
+  echo " "
+  echo "Vnc Server address will run at 127.0.0.1:5901"
+  echo " "
+  echo "Command to start 'vnc-start' and stop 'vnc-stop'"
+  echo " "
 rm termux-xfce.sh
-bash desktop-xfce
